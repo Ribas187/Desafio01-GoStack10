69129e84399f1d098c84987d3e13e91d6d2c3f31
Guilherme Ribas <guilherme.ribas.tech@gmail.com>
2020-01-27 17:32:05 -0300

    Middlewares added

--- index.js
+++ index.js
@@ -6,12 +6,35 @@ server.use(express.json());
 
 const projects = [
   {
-    id: "1",
+    id: 1,
     title: "Novo projeto",
     tasks: ["Nova tarefa"]
   }
 ];
 
+var requests = 0;
+
+
+//Middlewares:
+function checkIdExists(req, res, next) {
+  const { id } = req.params;
+  const index = projects.findIndex(item => item.id == id);
+
+  if (index < 0) {
+    return res.status(400).json({ error: 'ID does not exists' });
+  }
+
+  req.index = index;
+
+  return next();
+}
+server.use((req, res, next) => {
+  requests++;
+
+  next();
+
+  console.log(`Número de requisições realizadas: ${requests}`);
+});
 
 
 // Rotas:
@@ -23,11 +46,10 @@ server.post('/projects', (req, res) => {
   return res.json(projects);
 });
 
-server.post('/projects/:id/tasks', (req, res) => {
-  const { id } = req.params;
+server.post('/projects/:id/tasks', checkIdExists, (req, res) => {
   const { title } = req.body;
 
-  const index = projects.findIndex(item => item.id == id);
+  const index = req.index;
 
   const tasks = projects[index].tasks;
   tasks.push(title);
@@ -41,21 +63,18 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
-server.put('/projects/:id', (req, res) => {
-  const { id } = req.params;
+server.put('/projects/:id', checkIdExists, (req, res) => {
   const project = req.body;
 
-  const index = projects.findIndex(item => item.id == id);
+  const index = req.index;
 
   projects[index] = project;
   
   return res.json(projects);
 });
 
-server.delete('/projects/:id', (req,res) => {
-  const { id } = req.params;
-
-  const index = projects.findIndex(item => item.id == id);
+server.delete('/projects/:id', checkIdExists, (req,res) => {
+  const index = req.index;
 
   projects.splice(index, 1);
 
